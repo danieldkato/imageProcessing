d43b5bf18e425d164749d358ba0ae05094167e8b
danieldkato <daniel.d.kato@gmail.com>
2016-10-27 12:57:32 -0400

    Update comments for movingBoxcarDFF.m

--- movingBoxcarDFF/movingBoxcarDFF.m
+++ movingBoxcarDFF/movingBoxcarDFF.m
@@ -3,7 +3,32 @@
 % OVERVIEW:
 % This function takes raw intensity traces put out by a segmentation step
 % and returns dF/F traces in which the baseline F is based on a moxing
-% boxcar average of frames around any given frame.
+% boxcar average of frames around any given frame. Processed data is
+% returned in 2 formats:
+
+% 1) a grand k x (t - m -n) activity matrix, where k is the number of ROIs
+% analyzed from the current grab, t is the number of frames analyzed from
+% the current grab, m is the number of frames before any given frame to
+% include in the baseline average for that frame, and n is the number of
+% frames after any given frame to include in the baseline average for that
+% frame.
+
+% 2) a c x 1 cell array containing activity parsed into individual trials
+% and sorted by condition, where c is the number of trial conditions to be
+% analyzed. Each element of the cell array is a k x p x u slab of data
+% corresponding to a single trial condition, where k is the number of ROIs
+% to be analyzed, p is the number of samples in the peri-stimulus period to
+% be analyzed for each trial, and u is the number of trials of the given
+% trial condition.
+
+
+% REQUIREMENTS:
+% 1) The MATLAB function trialsByCondition.m, avaialable at 
+% https://github.com/danieldkato/trial_registration/blob/master/trialsByCondition.m
+
+% Note that this function treats the peri-stimulus period as a fixed number
+% of frames, which is really only sensible if the stimulus period of every
+% trial is the same.
 
 
 % INPUTS:
@@ -17,27 +42,64 @@
 % 3) n - the number of frames after the current frame included in the
 % moving boxcar average.
 
+% 4) Trials - an s x 3 cell array, where s is the number of trials to be
+% analyzed. The first column of each row gives the sample start number of a
+% given trial, the second column gives the name of the trial condition, and
+% the third column gives the trial duration. Note that this fucntion
+% assumes that Trials includes a 7-row header. 
+
+% 5) preStim - the number of samples before stimulus onset to include in
+% trial-by-trial plots.
+
+% 6) postStim - the number of samples after stimulus onset to include in
+% trial-by-trial plots. 
+
+% 7) outputDir - directory to which to save outputs.
+
+% 8) condSettingsPath - path to a .txt file contaning information about
+% different trial conditions used in the current experiment. This text file
+% should contain a series of MATLAB statements defining a c x 1 cell array
+% of structs called Conditions, where c is the number of trial conditions
+% to be analyzed from the current grab. Each struct should minimally
+% include fields corresponding to whatever trial parameters are necessary
+% for defining a condition in the current experiment, as well as a concise
+% condition name that will be assigned to each trial and that can be used
+% later on to easily parse experiments by condition. Example contents of a
+% conditionSettings.txt file might include:
+
+% Conditions{1}.STPRIDX = 1;
+% Conditions{1}.SPKRIDX = 0;
+% Conditions{1].Name = ' stepper only ';
+
+% Conditions{2}.STPRIDX = 0;
+% Conditions{2}.SPKRIDX = 1;
+% Conditions{2}.Name = ' speaker only ';
+
 
 % OUTPUTS:
-% 1) dFF - K x T matrix of dF/F matrix, where K is the number of ROIs and T is
-% the number of frames in the input grab. Note that because the baseline
-% for each frame is computed from m frames before and n frames after that
-% frame, we cannot compute dF/F for the first m or last n frames. The
-% columns corresponding to these frames are therefore replaced with NaNs.
+% 1) dFF - a grand k x (t - m -n) activity matrix, where k is the number of ROIs
+% analyzed from the current grab, t is the number of frames analyzed from
+% the current grab, m is the number of frames before any given frame to
+% include in the baseline average for that frame, and n is the number of
+% frames after any given frame to include in the baseline average for that
+% frame.
 
-% In addition to returning dFF formally, this function also saves dFF to
-% disk as a K x (T-pre-post) .csv for use in subsequent processing steps.
+% 2) TBC - a c x 1 cell array containing activity parsed into individual trials
+% and sorted by condition, where c is the number of trial conditions to be
+% analyzed. Each element of the cell array is a k x p x u slab of data
+% corresponding to a single trial condition, where k is the number of ROIs
+% to be analyzed, p is the number of samples in the peri-stimulus period to
+% be analyzed for each trial, and u is the number of trials of the given
+% trial condition.
+
+% In addition to returning dFF and TBC formally, this function also saves
+% dFF to storage as a .csv and an HDF5, and saves TBC as an HDF5.
 
 %%
-function dFF = movingBoxcarDFF(input, m, n, Trials, preStim, postStim, outputDir, condSettingsPath)
+function [dFF, TBC] = movingBoxcarDFF(input, m, n, Trials, preStim, postStim, outputDir, condSettingsPath)
     dat = csvread(input); % Load the data
     numROIs = size(dat, 1);
     numFrames = size(dat, 2);
-    
-    % Load condition settings:
-    fid = fopen(condSettingsPath);
-    content = fscanf(fid, '%c');
-    eval(content) % This is where conditions should be loaded in
     
     % Load trial data:
     [num, txt, trials] = xlsread(Trials);
@@ -73,15 +135,11 @@ function dFF = movingBoxcarDFF(input, m, n, Trials, preStim, postStim, outputDir
     dFFpadded = [NaN(numROIs, m), dFF, NaN(numROIs, n)];
     
     
-    %% Parse the data into individual trials, and organize by condition:
+    %% Parse the data into individual trials and organize by condition:
     
-    % IMPORTANT TO REMEMBER: the first column of trials has indices into
-    % input; but we've removed the first m columns of input (as well as the
-    % last n columns) to create dFF; so the indices in trials must be
-    % offset:
-    trials(:,1) = cellfun(@(c) c-m, trials(:,1), 'UniformOutput', 0);
+    trials(:,1) = cellfun(@(c) c-m, trials(:,1), 'UniformOutput', 0); % <-- REMEMBER!! we've removed the first m frames of the data, so trial start indices must be offset appropriately
+    TBC = trialsByCondition(dFF, trials, preStim, postStim, condSettingsPath); 
     
-    TBC = trialsByCondition(dFF, trials, preStim, postStim, Conditions); 
     
     %% Save the outputs to disk:
     status = exist(outputDir, 'dir');
