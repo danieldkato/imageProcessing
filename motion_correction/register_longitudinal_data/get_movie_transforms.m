d0f4442c647279696419676fe8f70c627fd65372
Dan Kato <dan@hs05bruno10.cpmc.columbia.edu>
2017-11-29 18:45:55 -0500

    Add get_movie_transforms.m

---
+++ motion_correction/register_longitudinal_data/get_movie_transforms.m
@@ -0,0 +1,40 @@
+function [Movies, mask] = get_movie_transforms(Movies)
+
+% Define the reference image; I'm arbitrarily making this the average image
+% of the first movie (I don't think it makes a difference which is the
+% reference):
+ref_img = Movies(1).mean_img;
+rfixed = imref2d(size(ref_img));
+
+% Create a mask that will be used to keep track of pixels that have to be
+% clipped (e.g., if a movie has to be translated down to be registered to a
+% reference movie, then the transformed movie B will have a black stripe at
+% the top);
+mask = ones(size(ref_img));
+
+% For each movie to be registered (i.e., every movie other than the
+% reference movie), find the transformation needed to match the reference
+% movie:
+disp('Finding affine transformation for each movie..');
+parfor m = 2:length(Movies)
+    disp(['find movie ' num2str(m) ' transform']);
+    Movies(m).transform = imregcorr(Movies(m).mean_img,ref_img); % estimate 2D affine transform for current movie
+    Movies(m).registered_avg = imwarp(Movies(m).mean_img, Movies(m).transform, 'OutputView', rfixed); % apply transformation to average image of current movie
+    
+    % If a pixel is clipped from any movie, then it must be excluded from
+    % ALL movies; e.g., if a transformed movie has a black stripe at the
+    % top because it had to be translated down to be registered to the
+    % reference movie, then we have to exclude the pixels corresponding to
+    % the location of the black stripe from EVERY other movie that we
+    % register
+    mask = mask & Movies(m).registered_avg ~= 0;
+    disp(['done finding movie ' num2str(m) ' transform' ]);
+end
+
+% In the special case of the reference movie, the transformation matrix is
+% just the identity matrix:
+Movies(1).transform = affine2d([1 0 0; 0 1 0; 0 0 1]);
+
+disp('... done finding affine transformation for each movie.');
+
+end
