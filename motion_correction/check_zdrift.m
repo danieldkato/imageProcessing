ca6e239c4a1b6e7311d7aa3a02c049a13e36f256
Dan Kato <dan@hs05bruno10.cpmc.columbia.edu>
2018-01-03 14:50:07 -0500

    In check_zdrift.m, start adding code for trying to register beginning- and end-movie mean images to z-stack, and save outputs to a new folder

--- motion_correction/check_zdrift.m
+++ motion_correction/check_zdrift.m
@@ -7,7 +7,7 @@
 % IV. INPUTS
 % V. OUTPUTS
 
-% Last updated DDK 2017-12-26
+% Last updated DDK 2018-01-03
 
 
 %% I. OVERVIEW
@@ -22,7 +22,9 @@
 
 
 %% II. REQUIREMENTS
-% MATLAB v >= ???
+% 1) The MATLAB function write_metadata.m, available at
+% https://github.com/danieldkato/utilities/blob/master/metadata/MATLAB/write_metadata.m.
+% Note that this function itself has a few dependencies.
 
 
 %% III. USAGE
@@ -80,12 +82,16 @@
 % 1) Provide ways of dealing with short (<2000 frames long) movies?
 
 
+%% CD to directory of movie:
+[directory, name, ext] = fileparts(which(path));
+cd(directory);
+
+
 %% Find number of frames in movie:
 tic;
 disp('Getting file info...');
 info = imfinfo(path);
 disp('... done'); toc;
-t = Tiff(path);
 num_frames = numel(info);
 width = info(1).Width;
 height = info(1).Height;
@@ -93,25 +99,27 @@ height = info(1).Height;
 
 %% Get average images of beginning and end of movie:
 
-% Get average of first thousand frames:
+image_data = Tiff(path);
+
+% Load first thousand frames and get average:
 tic;
 disp('Computing mean of first 1000 frames...');
 F = zeros(height, width, 1000);
 for i = 201:1200
-    t.setDirectory(i)
-    F(:, :, i-200) = double(t.read());
+    image_data.setDirectory(i)
+    F(:, :, i-200) = double(image_data.read());
 end
 avg_first = mean(F, 3); 
 disp('... done.');
 toc;
 
-% Get average of last thousand frames:
+% Load last thousand frames and get average:
 tic;
 L = zeros(height, width, 1000);
 disp('Computing mean of last 1000 frames...');
 for j = num_frames-999:num_frames
-    t.setDirectory(j)
-    L(:, :, j-(num_frames-1000)) = double(t.read());
+    image_data.setDirectory(j)
+    L(:, :, j-(num_frames-1000)) = double(image_data.read());
 end
 disp('... done');
 avg_last = mean(L, 3);
@@ -149,7 +157,7 @@ avg_last_reg = imwarp(avg_last, tform, 'OutputView', rfixed);
 if tform.T(3,1) > 0 
     first_col = 1 + ceil(tform.T(3,1));
     last_col = width;
-elseif tform.T(3,1) < 0
+elseif tform.T(3,1) <= 0
     first_col = 1;
     last_col = width - ceil(abs(tform.T(3,1)));
 end
@@ -157,7 +165,7 @@ end
 if tform.T(3,2) > 0 
     first_row = 1 + ceil(tform.T(3,2));
     last_row = height;
-elseif tform.T(3,2) < 0
+elseif tform.T(3,2) <= 0
     first_row = 1;
     last_row = height - ceil(abs(tform.T(3,2)));
 end
@@ -176,6 +184,7 @@ disp(r);
 % Sum of squared errors:
 sse = sum(sum((avg_last_reg - avg_first).^2));
 disp(sse);
+
 
 %% Compute correlaton map:
 %{
@@ -231,26 +240,125 @@ imshow(corr_map);
 %}
 
 
+%% Try to register the mean images to the z-stack:
+
+disp('Attempting to register average images to z-stack...');
+
+% Find if there's a z-stack directory associated with the imaging site:
+
+% CD to the site directory ad get contents:
+movie_dir = cd('../..'); % assuming a directory structure of /mouse/session/site/grab/2P/movie.tif, this should cd to the site directory
+ls = dir();
+
+% Check if directory contents include zstack directory:
+names = arrayfun(@(x) x.name, ls, 'UniformOutput', false);
+is_zdir = cellfun(@(c) regexp(c, 'zstack'), names, 'UniformOutput', false);
+zstack_exists = ~isempty(cell2mat(is_zdir));
+
+% If zstack exists, try to register beginning and end average images to plane of z-stack:
+if zstack_exists
+    site_dir = cd('zstack');
+    
+    % Get the name of the actual z-stack file:
+    ls = dir();
+    names = arrayfun(@(x) x.name, ls, 'UniformOutput', false);
+    is_zstack = cellfun(@(c) ~isempty(regexp(c, 'file_[0-9]*.tif', 'ONCE')), names, 'UniformOutput', false);
+    
+    % TODO: deal with situations where there's more than one regexp match?
+    % TODO: deal with situations where there's no regexp match?
+    z_idx = find(cell2mat(is_zstack)); 
+    zstack_name = ls(z_idx).name;
+    
+    % Get z-stack metadata:
+    z_metadata = loadjson('metadata.json'); 
+    % TODO: throw warning if metadata doesn't exist
+    % TODO: validate metadata
+    disp('Getting z-stack metadata...')
+    zinfo = imfinfo(zstack_name);
+    num_zframes = length(zinfo);
+    frames_per_slice = z_metadata.framesPerSlice;  
+    disp('... done.')
+    
+    % Load the z-stack image data:
+    disp('Loading z-stack image data...');
+    z_tiff = Tiff(zstack_name);
+    Z = NaN(zinfo(1).Height, zinfo(2).Width, num_zframes);
+    for slice = 1:num_zframes
+        z_tiff.setDirectory(slice);
+        Z(:,:,slice) = z_tiff.read(); 
+    end
+    disp('... done');
+    
+    % If the z-stack inclues more than one frame per slice, average together frames
+    % from each slice
+    ('Averaging frames from the same slice...');
+    if frames_per_slice > 1
+        if mod(num_zframes, frames_per_slice) == 0 % confirm that the recorded number of frames per slice divides evenly into the number of frames in the z-stack
+            
+            num_slices = num_zframes/frames_per_slice;
+            Z_avg = NaN(zinfo(1).Height, zinfo(2).Width, num_slices);
+            
+            ssi = frames_per_slice * ones(1, num_slices);
+            slice_start_indices = cumsum(ssi) - slices_per_frame + 1;
+            
+            for i = 1:length(slice_start_indices)
+                Z_avg(:,:,i) = mean(Z(:,:,slice_start_indices(i):start_slice_indices(i)+frames_per_slice-1),3);
+            end
+            
+            disp('... done');
+            
+        else
+            warn('Frames per slice must divide evenly into number of frames in z-stack; check that frames per slice recorded in z-stack metadata file is correct; skipping registration of average images to z-stack');
+        end
+        
+    end
+    
+    
+end
+
+
 %% Save outputs:
+
+% Check if output directory exists, and if not, create it and cd into it:
+ls = dir();
+names = arrayfun(@(x) x.name, ls, 'UniformOutput', false);
+is_cz = cellfun(@(c) regexp(c, 'check_zdrift'), names, 'UniformOutput', false);
+cz_exists = ~isempty(cell2mat(is_cz));
+if ~cz_exists
+    mkdir('check_zdrift');
+end
+old = cd('check_zdrift');
 
 % Create an output structure including the R value, mean beginning image,
 % mean end image, and difference
 D.r = r;
 D.sse = sse;
-D.beginnig = avg_first_adjusted;
+D.beginning = avg_first_adjusted;
 D.end = avg_last_adjusted;
 D.diff_map = diff_map;
 save('zdrift.mat', 'D');
 
-% Save images:
-imwrite(avg_first_adjusted, 'AVG_frames_201-1200_adjusted.tif');
-imwrite(avg_last_adjusted, ['AVG_frames_' num2str(num_frames - 999) '-' num2str(num_frames) '_adjusted.tif']);
+% Save average images:
+f_name = 'AVG_frames_201-1200_adjusted.tif';
+l_name = ['AVG_frames_' num2str(num_frames - 999) '-' num2str(num_frames) '_adjusted.tif'];
+
+imwrite(avg_first_adjusted, f_name);
+imwrite(avg_last_adjusted, l_name);
 imwrite(diff_map, 'dif_map.tif');
+
+
+%% Save metadata:
+
+Metadata.inputs(1).path = which(path);
+Metadata.outputs(1).path = which(f_name);
+Metadata.outputs(2).path = which(l_name);
+
+write_metadata(Metadata,'check_zdrift_metadata.json');
+
+cd('..');
 
 %{
 % Display images adjacent to each other;
 figure();
 imshowpair(avg_first, avg_last_reg, 'montage');
 %}
-
-%% Save metadata:
