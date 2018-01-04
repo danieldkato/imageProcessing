3f34d704bf6e64c104926e135020dd8566cff4cc
Dan Kato <dan@hs05bruno10.cpmc.columbia.edu>
2018-01-03 21:25:31 -0500

    Change process_zstack.m so that it doesn't have to load in entire z-stack at once

--- check_zdrift/process_zstack.m
+++ check_zdrift/process_zstack.m
@@ -34,10 +34,11 @@ Z_processed = [];
 
 
 %% Get z-stack metadata:
-disp('Getting z-stack metadata...')
-z_metadata = loadjson('metadata.json'); 
 % TODO: throw warning if metadata doesn't exist
 % TODO: validate metadata
+% TODO: include code to ensure that the proper metadata file is read
+disp('Getting z-stack metadata...')
+z_metadata = loadjson('metadata.json'); 
 zinfo = imfinfo(path);
 num_zframes = length(zinfo);
 frames_per_slice = z_metadata.framesPerSlice;  
@@ -45,6 +46,7 @@ disp('... done.')
   
 
 %% Load the z-stack image data:
+%{
 disp('Loading z-stack image data...');
 z_tiff = Tiff(path);
 Z = NaN(zinfo(1).Height, zinfo(2).Width, num_zframes);
@@ -53,29 +55,37 @@ for slice = 1:num_zframes
     Z(:,:,slice) = z_tiff.read(); 
 end
 disp('... done');
-    
+%}
+
 
 %% Average together frames from each slice:
 ('Averaging frames from the same slice...');
 
-if frames_per_slice > 1
-    if mod(num_zframes, frames_per_slice) == 0 % confirm that the recorded number of frames per slice divides evenly into the number of frames in the z-stack
+if mod(num_zframes, frames_per_slice) == 0 % confirm that the recorded number of frames per slice divides evenly into the number of frames in the z-stack
 
-        num_slices = num_zframes/frames_per_slice;
-        Z_processed = NaN(zinfo(1).Height, zinfo(2).Width, num_slices);
+    num_slices = num_zframes/frames_per_slice;
+    Z_processed = NaN(zinfo(1).Height, zinfo(2).Width, num_slices);
 
-        ssi = frames_per_slice * ones(1, num_slices);
-        slice_start_indices = cumsum(ssi) - slices_per_frame + 1;
+    ssi = frames_per_slice * ones(1, num_slices);
+    slice_start_indices = cumsum(ssi) - slices_per_frame + 1;
 
-        for i = 1:length(slice_start_indices)
-            Z_processed(:,:,i) = mean(Z(:,:,slice_start_indices(i):start_slice_indices(i)+frames_per_slice-1),3);
+    for i = 1:length(slice_start_indices)
+
+        tmp = NaN(zinfo(1).Height, zinfo(2).Width, frames_per_slice);
+
+        start_slice = slice_start_indices(i);
+        end_slice = slice_start_indices(i) + frames_per_slice - 1;
+        for ii = start_slice:end_slice
+            Z_tiff.setDirectory(ii);
+            tmp(:,:,ii-start_slice+1) = Z_tiff.read();
         end
 
-        disp('... done');
-
-    else
-        warn('Frames per slice must divide evenly into number of frames in z-stack; check that frames per slice recorded in z-stack metadata file is correct; skipping registration of average images to z-stack');
+        Z_processed(:,:,i) = mean(tmp,3);
     end
-else 
-    Z_processed = Z; 
-end    
+
+    disp('... done');
+
+else
+    warn('Frames per slice must divide evenly into number of frames in z-stack; check that frames per slice recorded in z-stack metadata file is correct; skipping registration of average images to z-stack');
+end
+    
