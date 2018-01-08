579e5adf02bf28611512aab42cfa5fd75ee3de03
Dan Kato <dan@hs05bruno10.cpmc.columbia.edu>
2018-01-08 18:22:01 -0500

    In ddk_scalpel_wrapper.R, fix bugs in how metadata is written

--- segmentation/scalpel/step0/ddk_scalpel0_wrapper.R
+++ segmentation/scalpel/step0/ddk_scalpel0_wrapper.R
@@ -68,11 +68,14 @@
 ####################################################################################################
 
 # Load necessary libraries:
-library("scalpel")
-library("rjson")
+library(scalpel)
+library(rjson)
+library(rhdf5)
 
 # Get path to parameters file from command line:
 args = commandArgs()
+
+start_time = Sys.time()
 
 # Load parameters: 
 jsondata <- fromJSON(file=args[2])
@@ -82,43 +85,80 @@ params <- jsondata$params
 raw_data_folder = params$raw_data_folder
 output_folder = params$output_folder
 video_height = params$video_height
+file_type = params$file_type
 
-setwd(raw_data_folder)
+# Make sure that output_folder and raw_rata_folder end in "/"
+if(substr(output_folder, nchar(output_folder), nchar(output_folder))!="/") output_folder = paste0(output_folder, "/")
+if(substr(raw_data_folder, nchar(raw_data_folder), nchar(raw_data_folder))!="/") raw_data_folder = paste0(raw_data_folder, "/")
+
+# Check if output directory exists, create if necessary, then CD to it
+dir.create(output_folder, showWarnings=TRUE)
+setwd(output_folder)
+
+# Get output file extension:
+if(file_type=="matlab"){
+	file_type_end =".mat"
+}else if(file_type=="R"){
+	file_type_end =".rds"
+}else if(file_type=="text"){
+	file_type_end=".txt"
+}else if(file_type=="HDF5"){
+	file_type_end=".h5"
+}else {
+	file_type_end=".txt.gz"
+}
+
+# Count number of input files:
+num_parts = 0
+while(file.exists(paste0(raw_data_folder, "Y_", num_parts+1, file_type_end))){
+	num_parts = num_parts + 1
+}
 
 # Perform scalpel step 0:
-step0out = scalpelStep0(rawDataFolder=raw_data_folder,outputFolder=output_folder,videoHeight=video_height,fileType="matlab")
+step0out = scalpelStep0(rawDataFolder=raw_data_folder,outputFolder=output_folder,videoHeight=video_height,fileType=file_type)
 
-# Save step0out object in most recent Step0 directory:
+# Save step0out object in most recent Step0 directory (scalpelStep0 should have just created one):
 step0_indices = unlist(grepl("Step0",dir())) # get indices of all directories containing the string "Step0"
 step0_dirs = dir()[step0_indices] # get the names of all directories containing the string "Step0"
 mtimes = file.info(step0_dirs)$mtime # get the time that each directory was last modified
 most_recent_step0 = step0_dirs[which.max(mtimes)] # find the most recently modified directory containing string "Step0"
 output_path = paste(most_recent_step0,"step0out.Rdata",sep="/")
 save(step0out,file=output_path)
-start = Sys.time()
-date = format(start,format="%Y-%m-%d")
-time = strftime(start,format="%H:%M:%S")
 
-# Save step 0 metadata:
+# Initialize step 0 metadata object:
 step0_metadata = list()
 
-input_path = paste(raw_data_folder,"Y_1.mat",sep="/")
-sys_out = system(paste("sha1sum",input_path,sep=" "),intern=TRUE)
-input_sha1 = substr(sys_out,1,40)
-input_1 = list(path=input_path,sha1=input_sha1)
-step0_metadata$inputs <- list(input_1)
-
+# Add scalpelStep0 parameters to metadata:
 step0_metadata$params$raw_data_folder = raw_data_folder
 step0_metadata$params$output_folder = output_folder
 step0_metadata$params$video_height = video_height
 
-output_full_path = paste(raw_data_folder,most_recent_step0,"step0out.Rdata",sep="/")
+# Define paths and compute SHA1 checksums for all input files, add to metadata:
+inputs <- vector(mode="list", length=num_parts)
+for(i in 1:num_parts){
+	input_path = paste0(raw_data_folder, "Y_", toString(i), file_type_end)
+	sys_out = system(paste("sha1sum",input_path,sep=" "),intern=TRUE)
+	input_sha1 = substr(sys_out,1,40)
+	inputs[[i]]<-list(path=input_path,sha1=input_sha1)
+}
+step0_metadata$inputs <- inputs
+
+# Define paths to outputs, add to metadata:
+output_full_path = paste(output_folder,most_recent_step0,"step0out.Rdata",sep="/")
 sys_out2 = system(paste("sha1sum",output_full_path,sep=" "),intern=TRUE)
 output_sha1 = substr(sys_out2,1,40)
 output_1 = list(path=output_full_path,sha1=output_sha1)
 step0_metadata$outputs <-list(output_1)
 
+# Define miscellaneous other metadata fields:
+end_time = Sys.time()
+date = format(start_time,format="%Y-%m-%d")
+time = strftime(end_time,format="%H:%M:%S")
+duration = as.double(end_time - start_time, units = "secs")
+
 step0_metadata$date = date
 step0_metadata$time = time
+step0_metadata$duration = duration
+
 m = toJSON(step0_metadata)
 write(m,file=paste(most_recent_step0,"metadata.json",sep="/"))
