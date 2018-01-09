ea8450a99de17f4380fbf3b697c060f61c65dcd3
Dan Kato <dan@hs05bruno10.cpmc.columbia.edu>
2018-01-08 20:06:22 -0500

    Modify fmt4scalpel_h5.m so that it can read from and write to a .mat or a .h5

--- segmentation/scalpel/fmt4scalpel_h5.m
+++ segmentation/scalpel/fmt4scalpel_h5.m
@@ -6,7 +6,7 @@ function fmt4scalpel_h5(varargin)
 % IV. INPUTS
 % IV. OUTPUTS
 
-% last updated DDK 2018-01-05
+% last updated DDK 2018-01-06
 
 
 %% I. OVERVIEW:
@@ -37,10 +37,11 @@ function fmt4scalpel_h5(varargin)
 
 
 %% IV. INPUTS:
-% 1) input_path - path to an HDF5 file to be vectorized. The imaging data to be
-% converted should be saved in a w x h x f dataset called '/mov', where w
-% is the width of the movie in pixels, h is the height of the movie in
-% pixels, and f is the number of frames in the movie. 
+% 1) input_path - path to a file containing data to be vectorized. The
+% imaging data to be converted should be saved in a w x h x f dataset
+% called '/mov', where w is the width of the movie in pixels, h is the
+% height of the movie in pixels, and f is the number of frames in the
+% movie.
 
 % 2) output_path - path where the output HDF5 file should be saved.  
 
@@ -75,11 +76,13 @@ function fmt4scalpel_h5(varargin)
 
 %% TODO:
 % 1) Throw warning if chunk_size is greater than the length of the movie
+% 2) Throw warning if no output_dir can be extracted from the specified
+% output path
 
 
 %% Define parameters:
 input_path = varargin{1};
-output_basepath = varargin{2};
+output_base = varargin{2};
 if nargin > 2
     chunk_size = varargin{3};
 else
@@ -91,33 +94,78 @@ else
     multiple_outputs = false;
 end
 
-fs = filesep;
-[dir, base, ext] = fileparts(output_basepath);
-if ~exist(dir)
-    mkdir(dir);
+
+%% Get format of input and output files:
+[input_dir, input_base, input_ext] = fileparts(input_path);
+[output_dir, output_name, output_ext] = fileparts(output_base);
+
+if ~exist(output_dir,'dir')
+    mkdir(output_dir);
 end
-old = cd(dir);
+
 
 %% Get the movie dimensions and number of frames:
 disp('Getting input file info...');
-info = h5info(input_path);
-height = info.Datasets(1).Dataspace.Size(1);
-width = info.Datasets(1).Dataspace.Size(2);
-num_frames = info.Datasets(1).Dataspace.Size(3); % assuming there's only one dataset; TODO; include code for when there are multiple datasets
+
+if strcmp(input_ext, '.h5')
+    info = h5info(input_path);
+    height = info.Datasets(1).Dataspace.Size(1);
+    width = info.Datasets(1).Dataspace.Size(2);
+    num_frames = info.Datasets(1).Dataspace.Size(3); % assuming there's only one dataset; TODO; include code for when there are multiple datasets
+elseif strcmp(input_ext, '.mat')
+    I = matfile(input_path);
+    height = size(I.mov,1);
+    width = size(I.mov,2);
+    num_frames = size(I.mov,3); % assuming there's only one dataset; TODO; include code for when there are multiple datasets
+end
+
 disp('... done');
 
 
+
 %% Create output HDF5 objects and dataset:
+
+% cd to directory where output files will be saved:
+old = cd(output_dir);
+
+disp(size(num_frames));
+disp(size(chunk_size));
 n_chunks = ceil(num_frames/chunk_size);
 
-disp('Creating output file...');
+disp('Creating output files...');
+% Setup output file if the user has requested only one output file... 
 if ~multiple_outputs
-    Outputs(1).name = h5create(output_basepath, '/mov', [height*width num_frames]);    
+    Outputs(1).name = output_base;
+    % ... if the user has requested an HDF5... 
+    if strcmp(output_ext, '.h5')    
+        h5create(Outputs(1).name, '/mov', [height*width num_frames]);    
+    % ... if the user has requested a .mat... 
+    elseif strcmp(output_ext, '.mat')
+        Outputs(1).matfile = matfile(Outputs(1).name,'Writable',true);
+        Outputs(1).matfile.mov = int16(zeros([height*width num_frames]));
+    end
+    
+% Setup output files if the user has requested multiple output files... 
 else
     for n = 1:n_chunks
-        Outputs(n).name = [base '_' num2str(n) ext];
+
+        % Define the current chunk size (the last chunk may be less that chunk_size)
+        if n < n_chunks
+            curr_chunk_size = chunk_size;
+        else
+            curr_chunk_size = mod(num_frames, chunk_size);
+        end        
+        
+        Outputs(n).name = [output_name '_' num2str(n) output_ext];
         disp(Outputs(n).name);
-        h5create(Outputs(n).name, '/mov', [height*width chunk_size]);    
+        % ... if the user has requested an HDF5... 
+        if strcmp(output_ext, '.h5')
+            h5create(Outputs(n).name, '/mov', [height*width chunk_size]);   
+        % ... if the user has requested a .mat... 
+        elseif strcmp(output_ext, '.mat')
+            Outputs(n).matfile = matfile(Outputs(n).name,'Writable',true);
+            Outputs(n).matfile.mov = int16(zeros([height*width chunk_size]));
+        end
     end
 end
 disp('... done');
@@ -126,30 +174,53 @@ disp('... done');
 %% Reshape data and write to new HDF5 files:
 for nn = 1:n_chunks
     
-    if nn < n_chunks
+    start_read_frame = (nn-1)*chunk_size+1; 
+    
+    % Define the current chunk size (the last chunk may be less that chunk_size)
+    if nn < n_chunks || mod(num_frames, chunk_size) == 0
         curr_chunk_size = chunk_size;
     else
         curr_chunk_size = mod(num_frames, chunk_size);
     end
 
+    % Determine the current output file (depends on whether the user has requested one or multiple output files, and on requested output type):
     if multiple_outputs 
-        start = [1 1];
-        curr_output_file = Outputs(n).name;
+        start_write_frame = 1;
+        if strcmp(output_ext, '.h5')
+           curr_output_file = Outputs(nn).name;
+        elseif strcmp(output_ext, '.mat')
+           curr_output_file = Outputs(nn).matfile;
+        end
     else
-        start = [1 (nn-1)*chunk_size+1];
-        curr_output_file = Outputs(1).name;
+        start_write_frame = start_read_frame;
+        if strcmp(output_ext, '.h5')
+          curr_output_file = Outputs(1).name;
+        elseif strcmp(output_ext, '.mat')
+          curr_output_file = Outputs(1).matfile;
+        end
     end
     
     disp(['Vectorizing frames ' num2str((nn-1)*chunk_size+1) ' to ' num2str((nn-1)*chunk_size+curr_chunk_size) ' out of ' num2str(num_frames)]);
     
+    % Read data from source:
     disp('    ... reading from input file...');
-    data = h5read(input_path, '/mov', [1 1 (nn-1)*chunk_size+1], [height width curr_chunk_size]); % Read data from source
+    if strcmp(input_ext, '.h5')    
+        data = h5read(input_path, '/mov', [1 1 start_read_frame], [height width curr_chunk_size]); 
+    elseif strcmp(input_ext, '.mat')
+        data = Outputs(nn).mat.mov(1:height, 1:width, start_read_frame:start_read_frame + curr_chunk_size - 1);
+    end
     
+    % Reshape data:
     disp('    ... reshaping data...');
-    data_reshaped = reshape(data, [height*width curr_chunk_size]); % Reshape
+    data_reshaped = reshape(data, [height*width curr_chunk_size]); 
     
-    disp('    ... writing to output file...');
-    h5write(curr_output_file, '/mov', data_reshaped, start, [height*width curr_chunk_size]); % Write data to output
+    % Write data to output:
+    disp(['    ... writing to  output file ' num2str(nn) '...']);
+    if strcmp(output_ext, '.h5')
+        h5write(curr_output_file, '/mov', data_reshaped, [1 start_write_frame], [height*width curr_chunk_size]); 
+    elseif strcmp(output_ext, '.mat')
+        curr_output_file.mov(:,start_write_frame:start_write_frame + curr_chunk_size -1) = data_reshaped; 
+    end
 end
 
 disp('Done.');
